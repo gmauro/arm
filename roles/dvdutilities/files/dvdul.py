d25005765e68c594dce67f0ea28de2bf79dce449
Gianmauro Cuccuru <gmauro@crs4.it>
2016-01-25 18:17:30 +0100

    putting oswitch into the workflow

--- roles/dvdutilities/files/dvdul.py
+++ roles/dvdutilities/files/dvdul.py
@@ -6,7 +6,8 @@ import sys
 
 def make_parser():
     parser = argparse.ArgumentParser(description='dvd_utils_launcher')
-    parser.add_argument('--action', type=str, choices=['backup', 'ripping'],
+    parser.add_argument('--action', type=str, choices=['backup', 'ripping',
+                                                       'bundle'],
                         help='actions', required=True)
     parser.add_argument('--source_drive', type=str, default='/dev/sr0',
                         help='source drive path (default /dev/sr0)')
@@ -30,30 +31,21 @@ def main(argv):
     def proc_to_exec(cmd):
         return subprocess.Popen(cmd.split(), stdout=subprocess.PIPE).communicate()[0]
 
-    parser = make_parser()
-    args = parser.parse_args(argv)
-
-    output_dir = os.path.expanduser(args.output_dir)
-
-    if args.action == 'backup':
-        cmd = "blkid -o value -s LABEL {}".format(args.source_drive)
+    def dvd_backup(input, output, config):
+        cmd = "blkid -o value -s LABEL {}".format(input)
         dvd_title = proc_to_exec(cmd).strip().replace(' ', '_')
-        with open("{}/{}".format(output_dir, args.config_file), 'w') as fp:
+        with open("{}/{}".format(output, config), 'w') as fp:
             fp.write(dvd_title)
-
-        cmd2 = "dvdbackup -i {} -o {} -M -n {}".format(args.source_drive,
-                                                       output_dir,
-                                                       dvd_title)
+        cmd2 = "dvdbackup -i {} -o {} -M -n {}".format(input, output, dvd_title)
         proc_to_exec(cmd2)
-
-        cmd3 = "eject {}".format(args.source_drive)
+        cmd3 = "eject {}".format(input)
         proc_to_exec(cmd3)
 
-    if args.action == 'ripping':
-        with open("{}/{}".format(output_dir, args.config_file), 'r') as fp:
+    def ripping(output, config, ext, preset):
+        with open("{}/{}".format(output, config_file), 'r') as fp:
             title = fp.readline().strip()
-            ipath = "{}/{}".format(output_dir, title)
-            ofile = "{}/{}.{}".format(output_dir, title, args.extension)
+            ipath = "{}/{}".format(output, title)
+            ofile = "{}/{}.{}".format(output, title, ext)
 
         cmd = """HandBrakeCLI \
                  -i {} \
@@ -64,11 +56,31 @@ def main(argv):
                  --audio 1,2,3 \
                  --aencoder copy:ac3 \
                  --audio-fallback ac3
-              """.format(ipath, ofile, args.handbrake_preset)
+              """.format(ipath, ofile, preset)
         proc_to_exec(cmd)
 
         shutil.rmtree("{}".format(ipath))
-        os.remove("{}/{}".format(output_dir, args.config_file))
+        os.remove("{}/{}".format(output, config))
+
+    parser = make_parser()
+    args = parser.parse_args(argv)
+
+    action = args.action
+    source_drive = args.source_drive
+    output_dir = os.path.expanduser(args.output_dir)
+    config_file = args.config_file
+    extension = args.extension
+    handbrake_preset = args.handbrake_preset
+
+    if action in ['backup', 'bundle']:
+        dvd_backup(source_drive, output_dir, config_file)
+
+    if args.action == 'ripping':
+        ripping(output_dir, config_file, extension, handbrake_preset)
+
+    if action == 'bundle':
+        cmd = "oswitch gmauro/arm dvdul ripping"
+        proc_to_exec(cmd)
 
 if __name__ == '__main__':
     main(sys.argv[1:])
